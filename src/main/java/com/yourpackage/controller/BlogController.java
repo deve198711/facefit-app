5c8631845b478b842379bad475a419832c24d527
Hocheol Lee <hocheollee@Hocheolui-MacBookAir.local>
2025-06-14 02:43:16 +0900

    Add blog post controller and template

---
+++ src/main/java/com/yourpackage/controller/BlogController.java
@@ -0,0 +1,13 @@
+package com.yourpackage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog/choosing-sunscreen")
+    public String showBlogPost1() {
+        return "blog-post1"; // templates 폴더 내 blog-post1.html 렌더링
+    }
+}
